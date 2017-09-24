37fd67a2b86768d76aba1e430a21a584f2d4d318
Philippe Muller <philippe.muller@gmail.com>
2017-09-24 18:20:08 +0800

    Allow newer requests versions

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
 	keywords=['bittrex', 'bitcoin', 'exchange'],
 	author='Rushy Panchal',
 	author_email='panchal.rushy@gmail.com',
-	install_requires=["requests==2.7.0"],
+	install_requires=["requests>=2.7.0"],
 	classifiers=[
 	    'Programming Language :: Python',
 	    'Programming Language :: Python :: 2.7',
